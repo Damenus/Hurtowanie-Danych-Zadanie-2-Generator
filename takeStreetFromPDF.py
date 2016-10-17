23730035eae310961fd1fe391b32864a8fdd34f8
Damenus <darczuk.damian@gmail.com>
2016-10-17 22:02:59 +0200

    Generated complet steet + district

--- takeStreetFromPDF.py
+++ takeStreetFromPDF.py
@@ -1,12 +1,28 @@
 import re
 
-int = 0
-
+count = 0
+output = open('streetAndDistrict.txt', 'w', encoding="utf8")
 with open('D:\Dysk Google\Studia\Semestr V\Hurtownie Danych\Hurtowanie Danych - Zadanie 2 - Generator\listOfStreet.txt', 'r+', encoding="utf8") as plik:
     for linia in plik.readlines():
-        int = int + 1
+        count = count + 1
+        staraLinia = linia.split(' ')
+        #lista = list(set(kolumny))
+        lista = list();
+        powtorzenie = 0
+        for i in reversed(staraLinia):
+            for j in lista:
+                if(i == j):
+                    powtorzenie = 1
+                    break
+            if(powtorzenie == 0):
+                lista.append(i)
+            powtorzenie = 0
 
-print(int)
+        for slowo in reversed(lista):
+            output.write(slowo + ' ')
 
-count = len(open('D:\Dysk Google\Studia\Semestr V\Hurtownie Danych\Hurtowanie Danych - Zadanie 2 - Generator\listOfStreet.txt', 'r+', encoding="utf8").readlines())
+
 print(count)
+output.close()
+#count = len(open('D:\Dysk Google\Studia\Semestr V\Hurtownie Danych\Hurtowanie Danych - Zadanie 2 - Generator\listOfStreet.txt', 'r+', encoding="utf8").readlines())
+#print(count)
