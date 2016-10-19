e6c36aa488ccad8a92abe6ed701fbd3bffb75ea5
Damenus <darczuk.damian@gmail.com>
2016-10-19 19:23:36 +0200

    Clear name + some SQL

--- insertSQLObywatel.py
+++ insertSQLObywatel.py
@@ -1,6 +1,6 @@
 from faker import Factory
-import PESELGenerator
-import IDCardGenerator
+import GeneratorPesel
+import GeneratorIDCard
 
 numberOfRecrods = 1000
 
@@ -10,10 +10,10 @@ output = open('insertSQLObywatel.txt', 'w', encoding="utf8")
 fake = Factory.create('pl_PL')
 
 for record in range(0,numberOfRecrods):
-    pesel = PESELGenerator.pesel()
+    pesel = GeneratorPesel.pesel()
     firstName = fake.first_name()
     lastName = fake.last_name()
-    idCardNumber = IDCardGenerator.idCard()
+    idCardNumber = GeneratorIDCard.idCard()
     fatherName = fake.first_name_male()
     output.write('INSERT INTO Obywatel (PESEL, Imie, Nazwisko, NumerDowoduTozsamosci, ImieOjca) VALUES (' + pesel + ', ' + firstName + ', ' + lastName + ', ' + idCardNumber + ', ' + fatherName + ');\n')
 
